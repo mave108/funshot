955b8f6f74a8d25590843c2f810b20abf3b14686
Rakesh <rakeshkumar8900@gmail.com>
2022-01-22 01:27:05 +0530

    primary color added to progress bar

--- components/progress-bar/internals.tsx
+++ components/progress-bar/internals.tsx
@@ -5,7 +5,7 @@ export const Progressbar: FC<ProgressbarProps> = ({progress}) => {
 
     return (
         <div className="w-full bg-gray-200 h-2 rounded-md">
-            <div className="bg-blue-600 h-2 rounded-md" style={{width: progress+'%'}}></div>
+            <div className="bg-primary h-2 rounded-md" style={{width: progress+'%'}}></div>
         </div>
 
     )
