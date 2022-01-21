ef25f9b54acd90f831c5e28dbb96b8137074c2f2
Rakesh <rakeshkumar8900@gmail.com>
2022-01-21 22:43:24 +0530

    modal with dynamic loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,25 @@
 import type { NextPage } from 'next'
+import dynamic from 'next/dynamic'
+import { useEffect, useState } from 'react';
 import Head from 'next/head'
 import { BaseLayout } from '../components/layouts/base/internals'
 import {VideoCard} from '../components/video-card';
+import { Spinner } from '../components/spinner';
+const Modal =  dynamic(() => import('../components/modal').then((mod) => mod.Modal),
+{ loading: () => <Spinner text='Loading...' /> , ssr: false }
+);
+
 
 
 const Home: NextPage = () => {
+  let [isModalOpen, toggleModal] = useState<boolean>(false);
+  
+  useEffect (()=> {       
+    document.addEventListener('addFunShot', function (e) { 
+      // console.log("funshot clicked")
+      toggleModal(true);
+    }, false);
+  });
   return (
     <BaseLayout>
       <Head>        
@@ -16,14 +31,17 @@ const Home: NextPage = () => {
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="language" content="English" />
         <meta name="author" content="Rakesh"></meta>
-      </Head>
+      </Head>      
         <VideoCard />
         <VideoCard />
         <VideoCard />
         <VideoCard />
         <VideoCard />
         <VideoCard />
-        <VideoCard />
+        <VideoCard />        
+            <Modal show={isModalOpen} title='Upload Video' overlay={true} close={()=> toggleModal(false)}>
+            kjkljkljlkj
+            </Modal>        
       </BaseLayout>
   )
 }
