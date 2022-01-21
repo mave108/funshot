3550bb3296125c0f4c9c95ae380eb6d4ff4eed9e
Rakesh <rakeshkumar8900@gmail.com>
2022-01-21 19:38:42 +0530

    meta tags added to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,15 @@ import {VideoCard} from '../components/video-card';
 const Home: NextPage = () => {
   return (
     <BaseLayout>
-      <Head>
-        <title>funShot</title>
+      <Head>        
+        <title>funShot</title>             
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>        
         <meta name="description" content="short fun videos" />
+        <meta name="keywords" content="fun,videos " />
+        <meta name="robots" content="noindex, nofollow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="author" content="Rakesh"></meta>
       </Head>
         <VideoCard />
         <VideoCard />
