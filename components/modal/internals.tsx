0ec1e784e734197ac39bdbb6c9b9056584464126
Rakesh <rakeshkumar8900@gmail.com>
2022-01-22 01:27:26 +0530

    close icon removed from modal

--- components/modal/internals.tsx
+++ components/modal/internals.tsx
@@ -1,7 +1,6 @@
 import { FC, Fragment, useRef, useState } from "react";
 import { ModalProps } from "./types";
 import { Dialog, Transition } from '@headlessui/react';
-import { XIcon} from '@heroicons/react/outline';
 import { Backdrop } from "../backdrop";
 
 export const Modal:FC<ModalProps> = ({title, show,children, backdrop, overlay, close}) => {
@@ -41,13 +40,9 @@ export const Modal:FC<ModalProps> = ({title, show,children, backdrop, overlay, c
           </Transition.Child> } 
         {backdrop && <Backdrop show />}
         <div className="min-h-screen px-4 text-center">                               
-        <div 
-        onMouseEnter={()=> setTimeout(()=> toggleCloseIcon(true), 300)} 
-        onMouseLeave={()=> setTimeout(()=> toggleCloseIcon(false), 300)}  
-        className="inline-block relative w-full max-w-2xl p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-xl">
-            {title && <Dialog.Title className='text-lg leading-6 font-medium text-gray-500'>{title}</Dialog.Title>}
-            {showCloseIcon && <XIcon className='absolute right-6 top-6 h-6 w-6 cursor-pointer' />}
-            {!showCloseIcon && <button className="absolute py-1 right-6 top-6 bg-white text-xs font-light text-gray-500 px-2 rounded-md shadow-sm border">ESC</button>}
+        <div className="inline-block relative w-full max-w-2xl p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-xl">
+            {title && <Dialog.Title className='text-lg leading-6 font-medium text-gray-500'>{title}</Dialog.Title>}            
+            <button onClick={close} className="absolute py-1 right-6 top-6 bg-white text-xs font-light text-gray-900 px-2 rounded-md shadow-sm border">ESC</button>
             <div className="flex w-full">
                 {children}
             </div>          
