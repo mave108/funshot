eb7063b027debd4b6e035abef9520a59cb120f50
Rakesh <rakeshkumar8900@gmail.com>
2022-01-22 22:31:05 +0530

    tags fetched from api + search by fuse.js

--- components/post/internals.tsx
+++ components/post/internals.tsx
@@ -1,16 +1,47 @@
-import { FC, useState } from "react";
-import { PostProps } from "./types";
+import { FC, useState, useEffect } from "react";
+import { PostProps, Tag } from "./types";
 import { Chip } from "../chips";
 import {PaperClipIcon, CloudUploadIcon} from '@heroicons/react/outline';
 import { Button, ButtonTypes } from "../button";
 import { Progressbar } from "../progress-bar";
+import { TextField } from "../text-field";
+import Fuse from 'fuse.js';
+import axios from "axios";
 
 export const Post:FC<PostProps> = ({}) => {
     let [isFileUploading, setFileUploadingState] = useState<boolean>(false);
+    let [tags, updateTags] = useState<Tag[]>([]);
+    let [filtertedTags, updateFilteredTags] = useState<Fuse.FuseResult<Tag>[]>();
+
+
+    useEffect(()=> {
+      axios.get('http://localhost:5001/api/tags').
+      then(({data}) => {
+         return data.data.tags;          
+      })
+      .then((tags = {}) =>{
+        const tagArr: Tag[]  = [];
+        for(var propName in tags) {
+            if(tags.hasOwnProperty(propName)) {                
+                tagArr.push({name: propName,id:  tags[propName]});                
+            }
+        }
+        updateTags(tagArr);
+      })
+    }, [])
+  
+    const autoSuggestTags = (e: React.ChangeEvent<HTMLInputElement>) => {        
+        const { value } = e.target;
+        console.log("typed tag", e.target.value)
+        const fuse = new Fuse(tags, {keys: ['name'], includeScore: true})
+        const serachResult = fuse.search(value);
+        updateFilteredTags(serachResult);                
+    }
+
     return (
         <div className='flex w-full flex-col justify-between border-[1px] shadow-sm rounded-md  mt-4'>
             <div className="py-2 px-4">
-                <input className='text-xl text-gray-500 focus:outline-none w-full' placeholder='Title' />
+                <TextField name="title" placeholder="Title" />
             </div>
             <div className="pb-2 px-4">
                 <textarea 
@@ -19,10 +50,14 @@ export const Post:FC<PostProps> = ({}) => {
                     rows={4}
                 ></textarea>
             </div>
-            <div className='pb-4 flex flex-wrap justify-end space-x-2 px-2'>            
-                <Chip text="#Assign" />          
+            <div className='pb-4 flex flex-wrap justify-between px-2'>    
+              <div className='w-1/4'><TextField name="title" placeholder="Tags" onChange={autoSuggestTags} /></div>
+              <div className='flex flex-row justify-end space-x-2'>
+              <Chip text="#Assign" />          
                 <Chip text="#Label" />          
-                <Chip text="#Due Date" />          
+                <Chip text="#Due Date" />    
+              </div>
+                      
             </div>            
             <div className='pl-4 pr-2 flex flex-wrap justify-between border-t-[1px] py-2 items-center'>
                 {!isFileUploading && <label htmlFor="file-upload" className="cursor-pointer">
