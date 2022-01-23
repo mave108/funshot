6414f672c915bdb7000949cfad1b009ce5ecea0b
Rakesh <rakeshkumar8900@gmail.com>
2022-01-24 00:30:54 +0530

    ExecuteOne utility

--- utils/genral.ts
+++ utils/genral.ts
@@ -1,3 +1,13 @@
 export function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(" ");
   }
+
+  export const ExecuteOne = (function() {
+    var executed = false;
+    return function(fn: any) {
+        if (!executed) {
+            executed = true;
+            fn();
+        }
+    };
+});
