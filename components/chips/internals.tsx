bb5e840dda9df959a367462709a625f2371250f2
Rakesh <rakeshkumar8900@gmail.com>
2022-01-23 22:32:45 +0530

    chip updated

--- components/chips/internals.tsx
+++ components/chips/internals.tsx
@@ -1,11 +1,59 @@
-import { FC } from "react";
-import { ChipProps } from "./types";
+import { FC, useEffect, useState } from "react";
+import classNames from "classnames";
+import { ChipProps, ChipType } from "./types";
 
-export const Chip: FC<ChipProps> = ({text}) => {
-
+export const Chip: FC<ChipProps> = ({
+    text, 
+    pulse = false, 
+    hoverText, 
+    onClick = () => {}, 
+    bounce = false, 
+    chipType = ChipType.DEFAULT,
+    children,
+    ...props
+ }) => {
+   
+    
     return (
-        <span className="px-4 py-2 rounded-full text-gray-400 bg-gray-100 font-semibold text-sm flex align-center w-max cursor-pointer active:bg-gray-300 transition duration-300 ease">
-            {text}
+        <span className={classNames(
+            'px-2 py-[0.5rem]',
+            'rounded-full',                        
+            'font-semibold',
+            'text-sm',
+            'flex',
+            'align-center',
+            'w-max',
+            'cursor-pointer',
+            'active:bg-gray-300',
+            'transition',
+            'duration-300',
+            'relative',
+            {'text-white' : chipType == ChipType.WARNING},
+            {'bg-orange-600': chipType == ChipType.WARNING},
+            {'hover:bg-orange-500': chipType == ChipType.WARNING},
+            {'text-gray-400' : chipType == ChipType.DEFAULT},
+            {'bg-gray-100': chipType == ChipType.DEFAULT},
+            {'hover:bg-gray-200': chipType == ChipType.DEFAULT},              
+            'ease',
+            {'animate-bounce': bounce}            
+
+        )}                                                                   
+            onClick={onClick}
+            title={hoverText}
+            {...props}
+            >
+            {children} {text}
+            {pulse && <Pulse />}
+        </span>
+    )
+}
+
+export const Pulse = () => {
+  
+    return (
+        <span className="flex h-3 w-3 absolute top-0 right-0 -mt-1 -mr-1">
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-blue-500 opacity-75"></span>
+            <span className="relative inline-flex rounded-full h-3 w-3 bg-primary "></span>
         </span>
     )
 }
--- components/chips/types.ts
+++ components/chips/types.ts
@@ -1,5 +1,14 @@
 import { ReactElement } from "react";
+export enum ChipType {
+   DEFAULT = 'DEFAULT',
+   WARNING = 'WARNING'
+}
 export interface ChipProps {
     text: string;
     icon?: ReactElement;
+    pulse?: boolean;
+    bounce?: boolean;
+    hoverText?: string;
+    chipType?: ChipType;
+    onClick?(item?: any): void;
 }
