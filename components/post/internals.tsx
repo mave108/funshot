5fbd326428765b1e57ad97b7b3a438ee812359bb
Rakesh <rakeshkumar8900@gmail.com>
2022-01-24 00:30:32 +0530

    tag functionality updated

--- components/post/internals.tsx
+++ components/post/internals.tsx
@@ -7,13 +7,17 @@ import { Progressbar } from "../progress-bar";
 import { TextField } from "../text-field";
 import Fuse from 'fuse.js';
 import axios from '../../utils/axios';
+import {v4 as uuid4} from 'uuid';
+import { ExecuteOne } from "../../utils/genral";
 
 export const Post:FC<PostProps> = ({}) => {
     let [typedTag, updateTypedTag] = useState<string>();
     let [isFileUploading, setFileUploadingState] = useState<boolean>(false);
     let [tags, updateTags] = useState<Tag[]>([]);
     let [selectedTags, updateSelectedTags] = useState<Tag[]>([]);
-    let [suggestedTags, updateSuggestedTags] = useState<Fuse.FuseResult<Tag>[]>([]);
+    let [suggestedTags, updateSuggestedTags] = useState<Tag[]>([]);
+    let [bounceOnce, setBounceState] = useState<boolean>(true);
+    let [tagError, updateTagError] = useState<boolean>(false);
 
 
     useEffect(()=> {
@@ -25,41 +29,58 @@ export const Post:FC<PostProps> = ({}) => {
     }, [])
   
     const autoSuggestTags = (e: React.ChangeEvent<HTMLInputElement>) => {        
-        const { value } = e.target;        
+        const { value } = e.target;                
         const fuse = new Fuse(tags, {keys: ['name'], includeScore: true, minMatchCharLength: 3})
-        const serachResult = fuse.search(value);
-        const revertSearchResult = [...serachResult].map(({item, ...rest}) => ({...rest,item: {...item,alert: false}}));
+        const serachResult = fuse.search(value).map(({item}) => ({...item}));        
         updateSuggestedTags(serachResult);
         updateTypedTag(value);
-        setTimeout(() => updateSuggestedTags(revertSearchResult), 4000);
+        //set alert false
+        // console.log("source tag", fuse.search(value));
+        // bounceOnce && setTimeout(() => updateSuggestedTags([...suggestedTags].map((tag) => ({...tag,alert: false}))), 4000);        
+        ExecuteOne()(() => setTimeout(() => {setBounceState(false); console.log("executeOnce")}, 400));
 
     }
 
     const addNewTag = (e: React.KeyboardEvent<HTMLInputElement>) => {        
         //on enter add new tag to the selected
-        if (e.key === 'Enter' && typedTag && typedTag?.length > 3) {
-            const isExist = selectedTags.find((tag) => tag.name == typedTag);            
-            if (isExist) {
-                const revertState = [...selectedTags];
-                const alertTags = [...selectedTags].map((tag) => ({...tag, ...(tag.id == isExist.id && {alert: true})}));                                
+        if (e.key === 'Enter' && typedTag && typedTag?.length >= 3) {
+            const existInSelected = selectedTags.find((tag) => tag.name == typedTag); 
+            //check of new tag is exist in source
+            const existInSuggestion = [...suggestedTags].find((tag) => tag.name == typedTag);                                    
+           
+            if (existInSuggestion) {
+                addOldTag(existInSuggestion);
+            }        
+            else if (existInSelected) {                
+                const alertTags = [...selectedTags].map((tag) => ({...tag, ...(tag.id == existInSelected.id && {alert: true})}));                
                 updateSelectedTags(alertTags);
                 //revert the state to stop alert
-                setTimeout(() => updateSelectedTags([...revertState]), 4000);
+                setTimeout(() => updateSelectedTags([...selectedTags].map((tag) => ({...tag, ...(tag.id == existInSelected.id && {alert: false})}))), 4000);
+                //empty suggested tag
+                updateSuggestedTags([]);                                                
             } else {
-                updateSelectedTags([...selectedTags, {id: '', name: typedTag, isNew: true, alert: false}]);
-                //empty the typed tag
-                updateTypedTag('');
+                //restrict to add 3 new tag only
+                const howManyNewTagAdded = selectedTags.reduce((accu,curr) =>  accu+= curr.isNew? 1 : 0,1); 
+                if (howManyNewTagAdded > 3) {
+                    //display error message
+                    updateTagError(true);
+                } else {
+                    updateSelectedTags([...selectedTags, {id: uuid4(), name: typedTag, isNew: true, alert: false}]);
+                    //empty the typed tag
+                    updateTypedTag('');
+                }
+                
             }
             
         } 
     }
-    const addOldTag = (tag: Fuse.FuseResult<Tag>) => {        
+    const addOldTag = (tag: Tag) => {        
         //remove from suggested
-        updateSuggestedTags([...suggestedTags.filter((suggestedTag) => suggestedTag.item.id != tag.item.id)])
+        updateSuggestedTags([...suggestedTags.filter((suggestedTag) => suggestedTag.id != tag.id)])
         //add to selected
-        updateSelectedTags([...selectedTags,{id: tag.item.id, name: tag.item.name, isNew: false, alert: false}])
+        updateSelectedTags([...selectedTags,{id: tag.id, name: tag.name, isNew: false, alert: false}])
         //remove from search source 
-        updateTags([...tags.filter((sourceTag) => sourceTag.id != tag.item.id)])
+        updateTags([...tags.map(({alert,...tag}) => ({...tag, alert: false})).filter((sourceTag) => sourceTag.id != tag.id)])
     }
 
     return (
@@ -76,8 +97,13 @@ export const Post:FC<PostProps> = ({}) => {
                 ></textarea>
             </div>
             <div className='flex flex-col'>
+                {tagError && <Chip 
+                chipType={ChipType.WARNING}
+                autoHide={false}
+                timeout={5000}
+                text="Only 3 new tags can be added, you can still choose from existing(the orange one)" />}
                 <div className='pb-4 flex flex-wrap justify-between px-2'>    
-                  <div className='w-1/4'>
+                  <div className='w-1/4 mb-2'>
                       <TextField 
                         name="title" 
                         placeholder="Tags" 
@@ -88,24 +114,25 @@ export const Post:FC<PostProps> = ({}) => {
                     </div>
                 <div className='flex flex-row justify-end space-x-2 space-y-2 flex-wrap'>
                     {
-                        selectedTags.map(({name,id, alert = false}) => <Chip 
+                        selectedTags.map(({name,id, alert = false}, index) => <Chip 
                         text={'#'+name} 
                         chipType={alert? ChipType.WARNING :ChipType.DEFAULT}
-                        key={id} 
+                        key={`stag-${index}-${id}`} 
                         hoverText="click to remove" 
                         bounce={alert}                        
                       ></Chip>
                     )}                                          
                 </div>                      
                 </div> 
-                <div className='flex flex-row flex-wrap justify-start space-x-2 px-2 py-2 '>
+                {/* <span className='text-sm text-gray-300'>Click to add</span> */}
+                <div className='flex flex-row flex-wrap justify-start space-x-2 px-2 py-2 '>                    
                    {
-                       suggestedTags.map((tags) => <Chip 
-                            text={'#'+ tags.item.name} 
-                            key={tags.item.id}   
-                            chipType={tags.item.alert? ChipType.WARNING :ChipType.DEFAULT}                          
-                            bounce= {tags.item.alert}
-                            onClick={() => addOldTag(tags)}
+                       suggestedTags.map((tag) => <Chip 
+                            text={'#'+ tag.name} 
+                            key={tag.id}   
+                            chipType={ChipType.WARNING}
+                            bounce= { bounceOnce}
+                            onClick={() => addOldTag(tag)}
                             hoverText="click to select"                                                        
                         />
                       )
