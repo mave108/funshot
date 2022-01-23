5af47dec1766bcdcc7dd0ab7e6b86514c67ef580
Rakesh <rakeshkumar8900@gmail.com>
2022-01-23 22:33:12 +0530

    text fielt comp updated

--- components/text-field/internals.tsx
+++ components/text-field/internals.tsx
@@ -1,16 +1,28 @@
 import { FC } from "react";
 import { TextFieldProps } from "./types";
 
-export const TextField: FC<TextFieldProps> = ({name, placeholder, defaultValue, disabled = false, onChange}) => {
+export const TextField: FC<TextFieldProps> = ({
+    name, 
+    placeholder, 
+    defaultValue, 
+    disabled = false, 
+    value = '',
+    onChange,
+    onKeyUp,
+    ...props
+  }) => {
 
     return (
         <input 
          name={name}
          defaultValue={defaultValue}
+         value={value}
          disabled={disabled}
          className='text-xl text-gray-500 focus:outline-none w-full disabled:cursor-not-allowed' 
          placeholder={placeholder} 
          onChange={onChange}
+         onKeyUp={onKeyUp}
+         {...props}
         />
     )
 }
--- components/text-field/types.ts
+++ components/text-field/types.ts
@@ -1,7 +1,13 @@
-export interface TextFieldProps {
+import React from "react";
+
+//React.HTMLFactory<HTMLInputElement>
+
+export interface TextFieldProps  {
    name: string;
    defaultValue?: string;
    placeholder?: string;
-   disabled?: boolean; 
+   disabled?: boolean;
+   value?: string; 
    onChange?(e: React.ChangeEvent<HTMLInputElement>): void;
+   onKeyUp?(e: React.KeyboardEvent<HTMLInputElement>): void;
 }
