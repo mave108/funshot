bd8d18210103d2d00d4a6dfd1ba349ce90d75b16
Rakesh <rakeshkumar8900@gmail.com>
2022-01-27 23:08:39 +0530

    shimmer implemented

--- components/video-card/internals.tsx
+++ components/video-card/internals.tsx
@@ -1,32 +1,89 @@
 import { FC } from "react";
 import { VideoCardProps } from './types';
-import Image from "next/image";
 import {UserIcon} from '@heroicons/react/outline';
+import { Chip } from "../chips";
+import classNames from "classnames";
 
-export const VideoCard: FC<VideoCardProps> = () => {
+export const VideoCard: FC<VideoCardProps> = ({
+  img,title = '', 
+  description = '', 
+  tags = [], 
+  user='Anonymous',
+  shimmer = false
+}) => {
 
     return (
         <div className="max-w-xl rounded overflow-hidden shadow-lg w-full bg-white mb-3">
             <div className='w-full flex justify-between p-3'>
-            <div className="flex">
-                <div className="rounded-full h-8 w-8 bg-gray-500 flex items-center justify-center overflow-hidden">
-                   <UserIcon className="inline-block h-6 w-6 rounded-full ring-1  text-white" /> 
-                </div>
-                <span className="pt-1 ml-2 font-bold text-sm">braydoncoyer</span>
+              <div className="flex space-x-2 items-center">
+                  <div className= {classNames(
+                    'rounded-full',
+                    'h-8',
+                    'w-8',
+                    'bg-slate-200',
+                    'flex',
+                    'items-center',
+                    'justify-center',
+                    'overflow-hidden',   
+                    {'animate-pulse': shimmer}                 
+                  )}
+                >
+                    {!shimmer && <UserIcon className="inline-block h-6 w-6 rounded-full ring-1  text-white" /> }
+                  </div>
+                    <span className={classNames(
+                      'pt-1',
+                      'ml-2 ',
+                      'font-bold',
+                      'text-sm',    
+                      {'rounded': shimmer},
+                      {'bg-slate-200': shimmer},  
+                      {'h-2': shimmer},   
+                      {'w-[10rem]': shimmer},           
+                    )}
+                    >{!shimmer && user}</span>
+              </div>
             </div>
+            <div className={classNames(
+                'flex',
+                'overflow-hidden',
+                'w-full',
+                'h-[20rem]',
+                'bg-no-repeat',
+                'bg-cover',
+                'bg-center',
+                {'animate-pulse': shimmer},
+                {'bg-slate-200': shimmer}
+              )}  
+              style={{backgroundImage: 'url('+(!shimmer && img)+')'}}></div>
+            <div className="px-6 py-4">
+                <div className={classNames(
+                  'font-bold',
+                  'text-xl',
+                  'mb-2',
+                  {'rounded': shimmer},
+                  {'w-1/2': shimmer},
+                  {'h-3': shimmer},
+                  {'bg-slate-200': shimmer}
+                )}
+                >{!shimmer && title}</div>
+              {!shimmer && <p className="text-gray-700 text-base">
+                    {description}                  
+                  </p>
+              }
+              {
+                shimmer && (
+                  <div className="mt-6">
+                    <p className="bg-slate-200 h-2 w-full rounded animate-pulse mb-2" />
+                    <p className="bg-slate-200 h-2 w-1/2 rounded animate-pulse mb-2" />
+                    <p className="bg-slate-200 h-2 w-1/3 rounded animate-pulse mb-2" />                     
+                    <p className="bg-slate-200 h-2 w-10/12 rounded animate-pulse" />                                    
+                  </div>
+                )
+              }
             </div>
-  <div className="flex overflow-hidden w-full h-[20rem] bg-no-repeat bg-cover bg-center" style={{backgroundImage: 'url(/scene.jpeg)'}}></div>
-  <div className="px-6 py-4">
-    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-    <p className="text-gray-700 text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-    </p>
-  </div>
-  <div className="px-6 pt-4 pb-2">
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-  </div>
-</div>
+        <div className="px-6 pt-4 pb-2 flex flex-row space-x-2 justify-end">
+          {tags.length > 0 && tags.map(({id, name}) => <Chip text={name} key={id} shimmer = {shimmer}  />)}    
+        </div>
+      </div>
     )
 }
--- components/video-card/types.ts
+++ components/video-card/types.ts
@@ -1,3 +1,12 @@
+export interface TagProps {
+    id: string;
+    name: string
+}
 export interface VideoCardProps {
-    
+    img: string;
+    title?: string;
+    user?: string
+    description?: string;
+    tags?: TagProps[]   
+    shimmer?: boolean;
 }
