64cc5bb6878264606a925239f6c5ebe2e03f4a37
Rakesh <rakeshkumar8900@gmail.com>
2022-02-03 17:37:06 +0530

    content type updated to json in axios

--- utils/axios.ts
+++ utils/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_END_POINT,
     timeout: 1000,
-    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    headers: {'Content-Type': 'application/json'}
   });
 
   export default instance;
