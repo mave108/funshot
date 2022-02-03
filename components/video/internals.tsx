a2f181804fa3e89518f3fa8aacf9115c464b24eb
Rakesh <rakeshkumar8900@gmail.com>
2022-02-03 17:36:22 +0530

    new comp to display video

---
+++ components/video/internals.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+import { VideoProps } from "./types";
+import classNames from 'classnames';
+
+
+export const Video: FC<VideoProps> = ({poster}) => {
+  
+    return (
+        <div className={classNames(
+            'flex',            
+            'w-full',            
+            'bg-no-repeat',
+            'bg-cover',
+            'bg-center',                        
+          )}  
+          style={{backgroundImage: 'url('+(poster)+')'}}></div>
+    )
+}
