28df3aaf27fc954a7b8ee6a30ff382457ae1cc51
Rakesh <rakeshkumar8900@gmail.com>
2022-02-03 17:35:48 +0530

    progress bar with the types

--- components/progress-bar/internals.tsx
+++ components/progress-bar/internals.tsx
@@ -1,11 +1,29 @@
 import { FC } from "react";
-import { ProgressbarProps } from "./types";
+import classNames from "classnames";
+import { ProgressbarProps, ProgressbarType } from "./types";
 
-export const Progressbar: FC<ProgressbarProps> = ({progress}) => {
+export const Progressbar: FC<ProgressbarProps> = ({progress, type = ProgressbarType.PRIMARY}) => {
 
     return (
-        <div className="w-full bg-gray-200 h-2 rounded-md">
-            <div className="bg-primary h-2 rounded-md" style={{width: progress+'%'}}></div>
+        <div className={
+            classNames(
+                'w-full',
+                'h-2',
+                'rounded-md',
+                'bg-gray-300'                                
+            )}
+        >
+            <div className={
+                classNames(
+                    'h-2',
+                    'rounded-md',
+                    {'bg-primary': type === ProgressbarType.PRIMARY},
+                    {'bg-orange-500': type === ProgressbarType.WARNING},
+                    {'bg-green-500': type === ProgressbarType.SUCCESS},
+                    {'bg-red-500': type === ProgressbarType.ERROR}
+
+                )}
+                style={{width: progress+'%'}}></div>
         </div>
 
     )
--- components/progress-bar/types.ts
+++ components/progress-bar/types.ts
@@ -1,3 +1,10 @@
+export enum ProgressbarType{   
+   PRIMARY = 'PRIMARY',
+   SUCCESS = 'SUCCESS',
+   WARNING = 'WARNING',
+   ERROR   = 'ERROR'
+}
 export interface ProgressbarProps{
     progress: number;
+    type ?: ProgressbarType
 }
