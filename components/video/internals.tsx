dfce5bb4585380075b52659773f0de7f29f44d76
Rakesh <rakeshkumar8900@gmail.com>
2022-02-14 23:16:39 +0530

    intersection observer added to check if video in in viewport to prevent unnecessary video play

--- components/video/internals.tsx
+++ components/video/internals.tsx
@@ -1,18 +1,51 @@
-import { FC } from "react";
+import { FC, useState, useRef, useEffect } from "react";
 import { VideoProps } from "./types";
 import classNames from 'classnames';
+import {PlayIcon} from '@heroicons/react/outline';
+// import Image from 'next/image';
 
+export const Video: FC<VideoProps> = ({poster, video}) => {
+    const [isVideoLoaded, setVideoLoaded] = useState<boolean>(false);
+    // const [isViewPort, updateInViewPortStatus] = useState<boolean>(false);
 
-export const Video: FC<VideoProps> = ({poster}) => {
-  
+    const videoref = useRef(null);
+   
+    useEffect(() => {
+        let options = {                        
+            threshold: 0
+          }
+        let observer = new IntersectionObserver((entries) => {
+            const { isIntersecting } = entries[0]; 
+            if(!isIntersecting ) {
+                setVideoLoaded(false);
+            }        
+        },options);
+        if (videoref && videoref.current) {            
+            observer.observe(videoref.current);
+        }        
+    }, []);
+
     return (
         <div className={classNames(
             'flex',            
             'w-full',            
             'bg-no-repeat',
             'bg-contain',
-            'bg-center',                        
+            'bg-center',  
+            'justify-center',
+            'relative',                                  
           )}  
-          style={{backgroundImage: `url(${poster})`}}></div>
+          ref={videoref}
+          >
+                {/* <Image alt="" src={poster} priority layout="intrinsic" height={320} width={400}/> */}
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                {!isVideoLoaded && <img src={poster} alt="" height="320px"/>}
+                {!isVideoLoaded && <div className='absolute rounded-md top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-100 px-2 cursor-pointer' ><PlayIcon 
+                onClick={()=> setVideoLoaded(true)} 
+                className='h-20 text-primary'/></div>}
+                {isVideoLoaded && <video height="320" width="570" controls autoPlay={true} >
+                    <source src={video} type="video/mp4" />
+                </video>}
+          </div>
     )
 }
